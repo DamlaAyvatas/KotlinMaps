ebb9164f5a65b214b0f8fdf34a1933606b7ec0b7
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-05-13 18:55:24 +0300

    add LocationManager and LocationListener

--- app/src/main/java/com/dayvatas/kotlinmaps/MapsActivity.kt
+++ app/src/main/java/com/dayvatas/kotlinmaps/MapsActivity.kt
@@ -1,7 +1,13 @@
 package com.dayvatas.kotlinmaps
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,6 +21,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager : LocationManager
+    private lateinit var locationListener : LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,9 +48,39 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+
+            }
+        }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
+
+
+
+
+/*
         // Add a marker in Amsterdam and move the camera
         val amsterdam = LatLng(52.370216,  4.895168)
         mMap.addMarker(MarkerOptions().position(amsterdam).title("Marker in Amsterdam"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(amsterdam))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(amsterdam,15f))
+*/
     }
 }
