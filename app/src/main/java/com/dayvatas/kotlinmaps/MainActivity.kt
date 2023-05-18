ee96e6587ade7b58b3a63d003e0f25ab7b4a9dea
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-05-18 23:26:59 +0300

    menu added to main activity

--- app/src/main/java/com/dayvatas/kotlinmaps/MainActivity.kt
+++ app/src/main/java/com/dayvatas/kotlinmaps/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.dayvatas.kotlinmaps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.place_menu, menu)     //menuyu bağladım
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.addPlace){
+            val intent = Intent(this@MainActivity, MapsActivity::class.java)
+            startActivity(intent)                       //menu içindeki itemi bağladım
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
