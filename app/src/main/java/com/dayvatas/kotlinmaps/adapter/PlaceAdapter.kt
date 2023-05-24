63a1c27e46fa1f4692c2d5b9897e8043e93d4b08
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-05-25 01:17:06 +0300

    recycler adapter added

---
+++ app/src/main/java/com/dayvatas/kotlinmaps/adapter/PlaceAdapter.kt
@@ -0,0 +1,32 @@
+package com.dayvatas.kotlinmaps.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dayvatas.kotlinmaps.databinding.RecyclerRowBinding
+import com.dayvatas.kotlinmaps.model.Place
+import com.dayvatas.kotlinmaps.view.MapsActivity
+
+class PlaceAdapter(val placeList : List<Place>) : RecyclerView.Adapter<PlaceAdapter.PlaceHolder> () {
+    class PlaceHolder(val recyclerRowBinding: RecyclerRowBinding) : RecyclerView.ViewHolder(recyclerRowBinding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceHolder {
+        val binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PlaceHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return placeList.size
+    }
+
+    override fun onBindViewHolder(holder: PlaceHolder, position: Int) {
+        holder.recyclerRowBinding.recyclerViewTextView.text = placeList.get(position).name
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, MapsActivity::class.java)
+            holder.itemView.context.startActivity(intent)
+        }
+    }
+}
